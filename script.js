fcf2e62df9c0f48048fe4f96fedc39ca08df1b79
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-11 17:25:51 -0300

    testando tarefa 2

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ function showText() {
   const containerText = document.querySelector('#meme-text');
   containerText.innerHTML = textInput.value;
 }
-
 const textInput = document.querySelector('#text-input');
 textInput.addEventListener('keyup', showText);
+
+// Adiciona imagem
+
+const memeInsert = document.querySelector('#meme-insert');
+memeInsert.addEventListener('change', () => {
+  const image = document.querySelector('#meme-image');
+  image.src = URL.createObjectURL(memeInsert.files[0]);
+});
