0ae570c9b3d826678d39025e09d1cb2c2aad3030
Malbathan Lima <malbathanlima@gmail.com>
2021-03-11 15:38:24 -0300

    adicao de divs de img input e txt

---
+++ script.js
@@ -0,0 +1,11 @@
+// containers de Input, img e text;
+const dadContent = document.body;
+const inputText = document.createElement('input');
+const divContainer = document.createElement('img');
+const textContainer = document.createElement('p');
+inputText.id = 'text-input';
+divContainer.id = 'meme-image-container';
+textContainer.id = 'meme-text';
+dadContent.append(inputText);
+dadContent.append(divContainer);
+divContainer.append(textContainer);
