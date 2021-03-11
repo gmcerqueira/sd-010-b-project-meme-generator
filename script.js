5073d396edbebf8a3b89ec7888e1fb4fa7141817
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-03-11 18:52:23 -0300

    finished adding ready memes and function, and border buttons

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const inputText = document.querySelector('#text-input');
 const inputImage = document.querySelector('#meme-insert');
 const containerText = document.querySelector('#meme-text');
 const containerImg = document.querySelector('#meme-image');
-
+const containerMeme = document.querySelector('#meme-image-container');
+const bgBtns = document.querySelectorAll('.button-container button');
+const rdyMemes = document.querySelectorAll('.ready-memes img');
 //             listeners
 
 // set inputText listener
@@ -22,11 +24,39 @@ const setInputImage = () => {
   });
 };
 
+// set backgroundcolor buttons listeners
+const bgColors = ['red', 'blue', 'green'];
+const setBgColorBtns = () => {
+  console.log(bgBtns);
+  bgBtns.forEach((btn, index) => {
+    const button = btn;
+    button.style.backgroundColor = bgColors[index];
+    btn.addEventListener('click', () => {
+      const size = btn.classList[0];
+      const type = btn.classList[1];
+      const color = btn.style.backgroundColor;
+      containerMeme.style.border = `${size} ${type} ${color}`;
+    });
+  });
+};
+
+// set Ready Memes
+
+const setRdyMemes = () => {
+  rdyMemes.forEach((img) => {
+    img.addEventListener('click', () => {
+      containerImg.src = img.src;
+    });
+  });
+};
+
 // loader
 
 const loadButtons = () => {
   setInputText();
   setInputImage();
+  setBgColorBtns();
+  setRdyMemes();
 };
 
 window.onload = () => {
