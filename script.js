a36a58c0fa7ab17b7052c98b6113b992d15481e0
Malbathan Lima <malbathanlima@gmail.com>
2021-03-11 16:33:06 -0300

    ajuste de tag img, para div em divContainer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // containers de Input, img e text;
 const dadContent = document.body;
 const inputText = document.createElement('input');
-const divContainer = document.createElement('img');
+const divContainer = document.createElement('div');
 const textContainer = document.createElement('p');
 inputText.id = 'text-input';
 divContainer.id = 'meme-image-container';
@@ -9,3 +9,9 @@ textContainer.id = 'meme-text';
 dadContent.append(inputText);
 dadContent.append(divContainer);
 divContainer.append(textContainer);
+
+function showText() {
+  textContainer.innerText = inputText.value;
+}
+
+inputText.addEventListener('keyup', showText);
