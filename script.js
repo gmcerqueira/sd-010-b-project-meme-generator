36f78a07147108879cde962d37514ed6501f648c
Jessie <miss_sc_jessi@hotmail.com>
2021-03-11 18:37:15 -0300

    requisito 4 100%

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function textoMeme() {
   const caixaTexto = document.querySelector('#text-input');
-  const textoMeme = document.querySelector('#meme-text');
-  caixaTexto.addEventListener('keyup', function () {
-    textoMeme.innerHTML = caixaTexto.value;
+  const textoMemes = document.querySelector('#meme-text');
+  caixaTexto.addEventListener('keyup', function showText() {
+    textoMemes.innerHTML = caixaTexto.value;
   });
 }
 
@@ -10,19 +10,19 @@ textoMeme();
 
 function imagemMeme() {
   const inputImagem = document.querySelector('#meme-insert');
-  const imagemMeme = document.querySelector('#meme-image');
-  inputImagem.addEventListener('change', function () {
+  const imagemMemes = document.querySelector('#meme-image');
+  inputImagem.addEventListener('change', function showPicture() {
     mostraFoto(this.files);
   });
 
   function mostraFoto(files) {
-      const leitor = new FileReader();
+    const leitor = new FileReader();
 
-      leitor.onload = function (event) {
-        imagemMeme.src = event.target.result;
-      };
+    leitor.onload = function leURL(event) {
+      imagemMemes.src = event.target.result;
+    };
 
-      leitor.readAsDataURL(files[0]);
+    leitor.readAsDataURL(files[0]);
   }
 }
 
