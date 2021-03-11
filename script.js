1ce61b96299cb2ba0b65d18da7ff7c43e235f850
Joao Vanelli <vanellijoao@gmail.com>
2021-03-11 17:03:48 -0300

    adicionando imagem / faltam alguns ajustes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Preencher caixa de texto enquanto o input é escrito
 
 const memeText = document.querySelector('#text-input');
-console.log(memeText);
 
 function textInput() {
   const textArea = document.querySelector('#meme-text');
@@ -10,3 +9,15 @@ function textInput() {
 }
 
 memeText.addEventListener('keyup', textInput);
+
+// Selecionando imagens
+// Referencia: https://www.webtrickshome.com/faq/how-to-display-uploaded-image-in-html-using-javascript
+
+const chooseImg = document.querySelector('#meme-insert');
+
+function loadFile(event) {
+  const image = document.getElementById('meme-image');
+  image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+chooseImg.addEventListener('change', loadFile);
