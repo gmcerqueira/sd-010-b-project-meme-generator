991655d812998c82a490d16923f78ac06b17e9e1
Malbathan Lima <malbathanlima@gmail.com>
2021-03-11 18:00:50 -0300

    mudança de tag de container de texto

--- script.js
+++ script.js
@@ -2,13 +2,21 @@
 const dadContent = document.body;
 const inputText = document.createElement('input');
 const divContainer = document.createElement('div');
-const textContainer = document.createElement('p');
+const textContainer = document.createElement('div');
+const imgContainer = document.createElement('img');
+const inputImg = document.createElement('input');
+inputImg.setAttribute('type', 'file');
+inputImg.id = 'meme-insert';
+imgContainer.setAttribute('type','image');
 inputText.id = 'text-input';
 divContainer.id = 'meme-image-container';
 textContainer.id = 'meme-text';
+imgContainer.id = 'meme-image';
 dadContent.append(inputText);
+dadContent.append(inputImg);
 dadContent.append(divContainer);
 divContainer.append(textContainer);
+divContainer.append(imgContainer);
 
 function showText() {
   textContainer.innerText = inputText.value;
