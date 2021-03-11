937991035c5790bf467d1963ede90b4ac46b9620
Thiago Marchini <thiago@msn.com>
2021-03-11 20:06:24 -0300

    pequenas mudanças

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-function drawImage() {
+function drawText(text) {
   const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const img = document.getElementById('background');
-  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  const context = canvas.getContext('2d');
+  context.font = '30px Arial';
+  context.fillText(text, 150, 350);
 }
 
-function drawText() {
-  const text = document.getElementById('text-input').value;
-  console.log(text);
+function drawBackground(image) {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  // context.clearRect(0, 0, canvas.width, canvas.height);
+  const img = image;
+  console.log(img);
+  context.drawImage(img, 0, 0);
+}
+
+const EL = (sel) => document.querySelector(sel);
+const ctx = EL('#canvas').getContext('2d');
+
+function readImage() {
+  if (!this.files || !this.files[0]) return;
+  const FR = new FileReader();
+  FR.addEventListener('load', (evt) => {
+    const img = new Image();
+    img.addEventListener('load', () => {
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.drawImage(img, 0, 0);
+    });
+    img.src = evt.target.result;
+  });
+  FR.readAsDataURL(this.files[0]);
 }
 
 window.onload = function () {
-  drawImage();
-  document.getElementById('insert-text').addEventListener('click', function() {
-    drawText();
-  });
-}
+  drawBackground(document.getElementById('background'));
+  drawText('Hello!!');
+};
+
+EL('#upload-image').addEventListener('change', readImage);
