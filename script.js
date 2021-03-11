1f84a3bf2f0885e9bc8b7ca58b887e93cc945f4a
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-11 20:25:41 -0300

    Realizando requisito 2

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+const insertImg = document.querySelector('#meme-insert');
+const memeImg = document.querySelector('#meme-image');
+
 textInput.addEventListener('input', () => {
   memeText.innerHTML = textInput.value;
+});
+
+insertImg.addEventListener('change', (event) => {
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
 });
