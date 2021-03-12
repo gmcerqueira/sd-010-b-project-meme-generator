e16093dcaaf5bbb27776b4cf299fc67360c69258
Thiago Marchini <thiago@msn.com>
2021-03-12 18:04:19 -0300

    alteracoes no script.js (ESLINT)

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function changeBorder(color) {
-  const colorChosen = color;
+document.getElementById('buttons').addEventListener('click', (event) => {
+  const buttonChosen = event.target.id;
   const container = document.getElementById('meme-image-container');
-  if (colorChosen === 'red') {
+  if (buttonChosen === 'fire') {
     container.style.border = '3px dashed red';
   }
-  if (colorChosen === 'blue') {
+  if (buttonChosen === 'water') {
     container.style.border = '5px double blue';
   }
-  if (colorChosen === 'green') {
+  if (buttonChosen === 'earth') {
     container.style.border = '6px groove green';
   }
-}
+});
 
 const paragraph = document.getElementById('meme-text');
 const text = document.getElementById('text-input');
-text.onkeyup = function () {
+text.onkeyup = function write() {
   if (text.value.length > 58) {
     alert('O limite máximo é de 60 caracteres!');
   } else {
@@ -25,9 +25,8 @@ text.onkeyup = function () {
 window.URL = window.URL || window.webkitURL || window.mozURL;
 const image = document.getElementById('meme-image');
 const insertImage = document.getElementById('meme-insert');
-insertImage.onchange = function () {
+insertImage.onchange = function uploadImage() {
   const urlImage = URL.createObjectURL(this.files[0]);
-  console.log(urlImage);
   image.src = urlImage;
 };
 
@@ -35,7 +34,4 @@ document.getElementById('memes').addEventListener('click', (event) => {
   const meme = event.target;
   const image2 = document.getElementById('meme-image');
   image2.src = meme.src;
-  console.log(meme);
-  console.log(image2);
-  console.log(meme);
 });
