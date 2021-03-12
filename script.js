b570e7a01fc90885ee5ca7bd326bbaba6ea5ac8c
Marcos Mantovani <mvmes23@gmail.com>
2021-03-12 19:13:05 -0300

    algumas fixes para os requisitos!

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-let inputText = document.querySelector(' #text-input ');
+const inputText = document.querySelector(' #text-input ');
 const spanText = document.querySelector(' #meme-text ');
-const imgContainer = document.querySelector('#meme-image-container');
 const inputImg = document.querySelector('#files');
-const img = document.querySelector('#img');
 const fakeBtn = document.querySelector('#fake-button');
-
 
 const getInputText = () => {
 	let text = inputText.value;
 	text = text.toUpperCase();
 	spanText.innerHTML = text;
-};
+	};
 
 const inputGivenText = () => {    
 	inputText.addEventListener('keyup' , () => {        
 	getInputText();
 	});   
-};
+	};
 
 const fakeBtnClick = () => {
 	fakeBtn.addEventListener('click' , () => {
 		inputImg.click();		
 	})
-}
+	}
 
 window.onload = () => {  
 	inputGivenText(); 
 	fakeBtnClick();	
-};
+	};
