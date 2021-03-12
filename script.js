25c28d333968ac12d73eadc67a9f12e443d698f0
Thiago Marchini <thiago@msn.com>
2021-03-12 10:49:06 -0300

    refeita a limitaçao de 60 caracteres, usando o JavaScript

--- script.js
+++ script.js
@@ -14,14 +14,18 @@ function changeBorder(color) {
 
 const paragraph = document.getElementById('meme-text');
 const text = document.getElementById('text-input');
-text.onkeyup = function() {
-  paragraph.textContent = text.value;
+text.onkeyup = function () {
+  if (text.value.length > 58) {
+    alert('O limite máximo é de 60 caracteres!');
+  } else {
+    paragraph.textContent = text.value;
+  }
 };
 
 window.URL = window.URL || window.webkitURL || window.mozURL;
 const image = document.getElementById('meme-image');
 const insertImage = document.getElementById('meme-insert');
-insertImage.onchange = function() {
+insertImage.onchange = function () {
   const urlImage = URL.createObjectURL(this.files[0]);
   console.log(urlImage);
   image.src = urlImage;
