3a6121f51329ffba38ab7ce114f3fa35501be036
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-11 21:03:44 -0300

    Realizando requisito 6

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const insertImg = document.querySelector('#meme-insert');
 const memeImg = document.querySelector('#meme-image');
+const styleBtns = document.querySelector('#styles-buttons');
 
 textInput.addEventListener('input', () => {
   memeText.innerHTML = textInput.value;
@@ -9,4 +10,14 @@ textInput.addEventListener('input', () => {
 
 insertImg.addEventListener('change', (event) => {
   memeImg.src = URL.createObjectURL(event.target.files[0]);
+});
+
+styleBtns.addEventListener('click', (event) => {
+  const imageBorder = document.getElementById('meme-image-container').style;
+
+  imageBorder.border = '';
+
+  if (event.target.id === 'fire') imageBorder.border = '3px dashed red';
+  if (event.target.id === 'water') imageBorder.border = '5px double blue';
+  if (event.target.id === 'earth') imageBorder.border = '6px groove green';
 });
