70d046e461f594c4bc73eda815daa1c42084c7ba
Flavio Andrade <foa12381@gmail.com>
2021-03-12 23:44:57 -0300

    adiciona input e  span

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const digitText = document.querySelector('#text-input');
+
 let loadFile = (event) => {
   let memeImage = document.getElementById('meme-image');
   memeImage.src = URL.createObjectURL(event.target.files[0]);
@@ -6,12 +8,12 @@ let loadFile = (event) => {
   };
 }
 
-let addText = function () {
-  let digitText =documentQuerySelector('#text-input')
+const addText = () => {
+  digitText.addEventListener('keyup', () => {
+  let spanDigit = document.querySelector('#meme-text');
+    spanDigit.innerHTML = digitText.value;
 
+  })
 }
+addText();
 
-let espelhoText = function () {
-  let espelhoTexto =document.querySelector('#meme-text')
-
-}
