fc7d8f083dc2bf4a0c2684c4e1769effed05bcff
riquee <strikedv@hotmail.com>
2021-03-13 13:18:58 -0300

    Upload imagem finalizado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputText = document.querySelector('#text-input');
-// const container = document.querySelector('#meme-image-container');
+const container = document.querySelector('#meme-image-container');
 const memeText = document.querySelector('#meme-text');
+const memeInsert = document.querySelector('#meme-insert');
 
 const text = (event) => {
   memeText.textContent = event.target.value;
@@ -10,4 +11,15 @@ const addText = () => {
   inputText.addEventListener('keyup', text);
 };
 
+const showImg = (event) => {
+  const img = document.createElement('img');
+  container.appendChild(img);
+  img.src = URL.createObjectURL(event.target.files[0]);
+};
+
+const insertImg = () => {
+  memeInsert.addEventListener('change', showImg);
+};
+
 addText();
+insertImg();
