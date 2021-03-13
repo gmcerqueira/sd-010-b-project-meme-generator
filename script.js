5bbc9ba75295ab0ee9d367c83413b5dc07309116
Malbathan Lima <malbathanlima@gmail.com>
2021-03-13 15:02:57 -0300

    ajustes CSS e adiçao de alert com o maximo de caracteres

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 // containers de Input, img e text;
 const dadContent = document.body;
+
+const inputContainer = document.createElement('div');
+const labelText = document.createElement ('label');
 const inputText = document.createElement('input');
-const divContainer = document.createElement('div');
-const textContainer = document.createElement('div');
-const imgContainer = document.createElement('img');
+const labelImg = document.createElement ('label');
 const inputImg = document.createElement('input');
+inputContainer.id = 'inputs';
+labelText.setAttribute('for','text-input');
+labelText.innerHTML = 'Texto:';
+labelImg.setAttribute('for', 'meme-insert');
+labelImg.innerHTML = 'Foto:';
 inputImg.setAttribute('type', 'file');
 inputImg.id = 'meme-insert';
 inputImg.setAttribute('accept','image');
 inputText.id = 'text-input';
+
+const divContainer = document.createElement('div');
+const textContainer = document.createElement('div');
+const imgContainer = document.createElement('img');
 divContainer.id = 'meme-image-container';
 textContainer.id = 'meme-text';
 imgContainer.id = 'meme-image';
 imgContainer.setAttribute('type', 'image');
-dadContent.append(inputText);
-dadContent.append(inputImg);
+dadContent.append(inputContainer);
+inputContainer.append(labelText);
+inputContainer.append(inputText);
+inputContainer.append(labelImg);
+inputContainer.append(inputImg);
 dadContent.append(divContainer);
 divContainer.append(textContainer);
 divContainer.append(imgContainer);
 
-function showText() {
-  textContainer.innerText = inputText.value;
-}
 
-inputText.addEventListener('keyup', showText);
+inputText.onkeyup = function write() {
+  if (inputText.value.length > 38) {
+    alert('O limite máximo é de 40 caracteres!');
+  } else {
+    textContainer.textContent = inputText.value;
+  }
+};
 
-function showImg() {
-  imgContainer.innerHTML = inputImg.value;
-  imgContainer[0].src =(URL.createObjectURL(imgContainer.files[0]));
-}
+window.URL = window.URL || window.webkitURL;
+  inputImg.onchange = function uploadImage() {
+    const urlImage = URL.createObjectURL(this.files[0]);
+    imgContainer.src = urlImage;
+};
 
-inputImg.addEventListener('change', showImg);
