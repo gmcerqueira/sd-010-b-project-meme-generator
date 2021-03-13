819d2711b706c6b083c228e244c866c8ab4db73d
Malbathan Lima <malbathanlima@gmail.com>
2021-03-13 16:28:43 -0300

    adicao de botoes e style

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 const dadContent = document.body;
 
 const inputContainer = document.createElement('div');
-const labelText = document.createElement ('label');
+const labelText = document.createElement('label');
 const inputText = document.createElement('input');
-const labelImg = document.createElement ('label');
+const labelImg = document.createElement('label');
 const inputImg = document.createElement('input');
 inputContainer.id = 'inputs';
-labelText.setAttribute('for','text-input');
+labelText.setAttribute('for', 'text-input');
 labelText.innerHTML = 'Texto:';
 labelImg.setAttribute('for', 'meme-insert');
 labelImg.innerHTML = 'Foto:';
 inputImg.setAttribute('type', 'file');
 inputImg.id = 'meme-insert';
-inputImg.setAttribute('accept','image');
+inputImg.setAttribute('accept', 'image');
 inputText.id = 'text-input';
 inputText.setAttribute('maxlength', '60');
 
@@ -33,7 +33,6 @@ dadContent.append(divContainer);
 divContainer.append(textContainer);
 divContainer.append(imgContainer);
 
-
 inputText.onkeyup = function write() {
   if (inputText.value.length > 58) {
     alert('O limite máximo é de 60 caracteres!');
@@ -43,8 +42,37 @@ inputText.onkeyup = function write() {
 };
 
 window.URL = window.URL || window.webkitURL;
-  inputImg.onchange = function uploadImage() {
-    const urlImage = URL.createObjectURL(this.files[0]);
-    imgContainer.src = urlImage;
+inputImg.onchange = function uploadImage() {
+  const urlImage = URL.createObjectURL(this.files[0]);
+  imgContainer.src = urlImage;
 };
+
+const buttonsContainer = document.createElement('div');
+const button1 = document.createElement('button');
+button1.id = 'fire';
+button1.innerHTML = 'Fire!';
+const button2 = document.createElement('button');
+button2.id = 'water';
+button2.innerHTML = 'Water!';
+const button3 = document.createElement('button');
+button3.id = 'earth';
+button3.innerHTML = 'Earth!';
+buttonsContainer.id = 'buttons';
+dadContent.append(buttonsContainer);
+buttonsContainer.append(button1, button2, button3);
+
+const buttonsBorder = document.querySelector('#buttons');
+buttonsBorder.addEventListener('click', (event) => {
+  const buttonChosen = event.target.id;
+  const container = document.querySelector('#meme-image-container');
+  if (buttonChosen === 'fire') {
+    container.style.border = '3px dashed red';
+  }
+  if (buttonChosen === 'water') {
+    container.style.border = '5px double blue';
+  }
+  if (buttonChosen === 'earth') {
+    container.style.border = '6px groove green';
+  }
+});
 
