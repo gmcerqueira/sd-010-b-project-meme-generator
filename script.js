bd0b12513b36f5cc42937b991685ab37d35e9066
denis-rossati <denis.rossati@hotmail.com>
2021-03-14 19:22:25 -0300

    tentativa requisito 2

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const inputText = document.getElementById('text-input');
+const inputImage = document.getElementById('meme-insert');
 const divText = document.getElementById('meme-text');
+const image = document.getElementById('meme-image');
 
 function inputValue() {
   inputText.addEventListener('keyup', () => {
     divText.innerText = inputText.value;
   });
 }
+function fillMemeImageContainer() {
+  window.URL = window.URL || window.webkitURL || window.mozURL;
+  inputImage.addEventListener('change', () => {
+    image.src = URL.createObjectURL(inputImage.files[0]);
+  });
+}
+
 window.onload = function pageFunctions() {
   inputValue();
+  fillMemeImageContainer();
 };
