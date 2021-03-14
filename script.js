71cc3dea38718d21c73bd3de5089d3191d173782
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-14 17:45:26 -0300

    add exercicio 2

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const input = document.getElementById('text-input');
+const inputText = document.getElementById('text-input');
 const textMeme = document.getElementById('meme-text');
+const inputImage = document.getElementById('meme-insert');
+const imageMeme = document.getElementById('meme-image');
 
 function inputTextMeme() {
-  input.addEventListener('keyup', () => {
-    textMeme.innerHTML = input.value;
+  inputText.addEventListener('keyup', () => {
+    textMeme.innerHTML = inputText.value;
   });
 }
 inputTextMeme();
+
+function inputImageMeme() {
+  inputImage.addEventListener('change', (event) => {
+    imageMeme.src = (URL.createObjectURL(event.target.files[0]));
+  });
+}
+inputImageMeme();
