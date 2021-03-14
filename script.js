c3d008f116dcbfe3897d37b8bfa08aecf6310f08
legalvao1 <legalvao1@gmail.com>
2021-03-14 19:37:19 -0300

    Arrumando as bordas

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let caixaTexto = document.querySelector('#text-input');
-let textoDigitado = document.querySelector('#meme-text')
+let textoDigitado = document.querySelector('#meme-text');
 
-function insereTexto(){
-    textoDigitado.innerHTML = caixaTexto.value;
+function insereTexto() {
+  textoDigitado.innerHTML = caixaTexto.value;
 }
-caixaTexto.addEventListener('input', insereTexto)
+caixaTexto.addEventListener('input', insereTexto);
 
-let file = document.querySelector('#meme-insert')
+let file = document.querySelector('#meme-insert');
 
 // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
 let loadFile = function(evento){
-    let imagem = document.getElementById('meme-image')
-    imagem.src = URL.createObjectURL(evento.target.files[0]);
-    imagem.onload = function() {
-        URL.revokeObjectURL(imagem.src)
-    }
+  let imagem = document.getElementById('meme-image');
+  imagem.src = URL.createObjectURL(evento.target.files[0]);
+  imagem.onload = function() {
+    URL.revokeObjectURL(imagem.src);
+  }
 };
     
 let memeImageContainer = document.getElementById('meme-image-container');
@@ -22,22 +22,32 @@ const fire = document.getElementById('fire');
 const water = document.getElementById('water');
 const earth = document.getElementById('earth');
 
-function corDaBorda(evento){
-    let cor = evento.target.id;
-    switch(cor){
-        case 'fire':
-            memeImageContainer.style.border = '3px dashed red';
-            break;
-        case 'water':
-            memeImageContainer.style.border = '5px double blue';
-            break;
-        case 'earth':
-            memeImageContainer.style.border = '6px groove green';
-            break;
-    }
-}
+// function corDaBorda(evento){
+//     let cor = evento.target.id;
+//     switch(cor){
+//         case 'fire':
+//             memeImageContainer.style.border = '3px dashed red';
+//             break;
+//         case 'water':
+//             memeImageContainer.style.border = '5px double blue';
+//             break;
+//         case 'earth':
+//             memeImageContainer.style.border = '6px groove green';
+//             break;
+//     }
+// }
 
-fire.addEventListener('click', corDaBorda)
-water.addEventListener('click', corDaBorda)
-earth.addEventListener('click', corDaBorda)
+function corDaBorda(evento) {
+  let cor = evento.target.id;
+  if (cor === 'fire') {
+    memeImageContainer.style.border = '3px dashed red';
+  } else if (cor === 'water'){
+    memeImageContainer.style.border = '5px double blue';
+  } else {
+    memeImageContainer.style.border = '6px groove green';
+  }
+}
+fire.addEventListener('click', corDaBorda);
+water.addEventListener('click', corDaBorda);
+earth.addEventListener('click', corDaBorda);
 
