a9d9fc5188b6db4628edf79c086c5333308a0b57
legalvao1 <legalvao1@gmail.com>
2021-03-14 14:18:30 -0300

    Arrumando requisitos 2 e 3

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ function insereTexto(){
 caixaTexto.addEventListener('input', insereTexto)
 
 let file = document.querySelector('#meme-insert')
-let caminhoImagem = document.querySelector('#meme-image')
-let imagem = document.getElementsByTagName('img')
 
-function insereImagem(){
-    console.log(imagem)
-    imagem[0].src = file.files.item(0).name;    
-}
-file.addEventListener('change', insereImagem)
+// https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+let loadFile = function(evento){
+    let imagem = document.getElementById('meme-image')
+    imagem.src = URL.createObjectURL(evento.target.files[0]);
+    imagem.onload = function() {
+        URL.revokeObjectURL(imagem.src)
+    }
+};
+    
+
+
