883c8c09ab7139a68f00481deed435a1c94d9c87
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-15 11:23:05 -0300

    2º requisito completo

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/* function insertText() {
-  
-} */
-
 function addEventTextInput() {
   const txtInput = document.getElementById('text-input');
   const memeText = document.getElementById('meme-text');
@@ -10,3 +6,22 @@ function addEventTextInput() {
   });
 }
 addEventTextInput();
+
+/*
+* Consultei o seguinte link como sugestão da Trybe para utilização da função de upload.
+* Link: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+ */
+
+function loadFile(event) {
+  const output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = () => {
+    URL.revokeObjectURL(output.src); // free memory
+  };
+}
+
+function addEventFileInput() {
+  const fileInput = document.getElementById('meme-insert');
+  fileInput.addEventListener('change', loadFile);
+}
+addEventFileInput();
