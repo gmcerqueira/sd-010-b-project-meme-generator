d9e35521fb39b1b430576a2b6e6d56f440b2b613
Thiago Kachinsky <tjk091@gmail.com>
2021-03-16 12:56:54 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const imgId = document.getElementById('meme-image');
 const inputId = document.getElementById('meme-insert');
 
 inputId.addEventListener('change', () => {
+  // FileReader - Permite aplicação ler conteúdos dos arquivos do computador do usuário
   const reader = new FileReader();
 
   if (inputId.files.length <= 0) {
@@ -16,8 +17,10 @@ inputId.addEventListener('change', () => {
   }
 
   reader.onload = () => {
+    // result - devolve o conteúdo do ficheiro, só e válida após a operação de leitura ser conncluída
     imgId.src = reader.result;
   };
 
+  // readAsDataURL - usado para ler o conteúdo do 'file'
   reader.readAsDataURL(inputId.files[0]);
 });
