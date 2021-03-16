495b7141279281a079cfb623ab2a3caf1d1ee0c6
Thiago Kachinsky <tjk091@gmail.com>
2021-03-16 12:49:11 -0300

    Requisitos 1 e 2

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const textP = document.getElementById('meme-text');
 textInput.addEventListener('keyup', () => {
   textP.innerHTML = textInput.value;
 });
+
+const imgId = document.getElementById('meme-image');
+const inputId = document.getElementById('meme-insert');
+
+inputId.addEventListener('change', () => {
+  const reader = new FileReader();
+
+  if (inputId.files.length <= 0) {
+    return;
+  }
+
+  reader.onload = () => {
+    imgId.src = reader.result;
+  };
+
+  reader.readAsDataURL(inputId.files[0]);
+});
