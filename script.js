1455ee073b4b7d21e376a0a12ce0334388deca5d
Daniel Ceci <danielceci96@gmail.com>
2021-03-16 14:15:12 -0300

    comitão. Com ajuda dos instrutores, consegui  vários requisitos de uma vez.

---
+++ script.js
@@ -0,0 +1,16 @@
+let input=document.getElementById('text-input')
+let memeText=document.getElementById('meme-text')
+input.addEventListener('input',sameSentence)
+function sameSentence(){
+   memeText.innerHTML= input.value
+}
+//requisito de carregar a foto ao apertar o botão 
+let fileInput=document.getElementById('meme-insert')
+
+ function uploadImage(event) {
+	var image = document.getElementById('meme-image-container');
+	let url=URL.createObjectURL(event.target.files[0]);
+    image.style.backgroundImage=`url(${url})`
+   
+};
+fileInput.addEventListener('input',uploadImage)
