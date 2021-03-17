845268b1424ba9532e07ebc228ff0305d634e9c0
root <diegoserafim1@gmail.com>
2021-03-17 18:42:02 -0300

    D2 trocando o meme-image por meme-insert

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const areaDigitavel = document.getElementById('text-input');
-// const fotoSelecionada = document.getElementById('input#meme-image');
+const fotoSelecionada = document.getElementById('input#meme-image');
 const retornaTextoDigitado = document.getElementById('meme-text');
 // console.log(retornaTextoDigitado + 'oi');
 // console.log(fotoSelecionada + 'oi');
 function inputDeEntrada() {
   const areaDigit = areaDigitavel.value;
   retornaTextoDigitado.innerHTML = areaDigit;
+}
+function fotoSelecionada() {
+  const fotoSelec = = 
 }
 window.onload = function inicio() {
   areaDigitavel.addEventListener('input', inputDeEntrada);
