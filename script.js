f5fe8e414edd2c3956215a075552b98c9ee62bd1
Marcos Mantovani <mvmes23@gmail.com>
2021-03-17 22:03:26 -0300

    new feature, change border at button click!

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const textInput = document.querySelector('#text-input');
 const textWriter = document.querySelector('#meme-text');
+const borderBtns = document.querySelector('#borderBtn-container');
+const memeImgContainer = document.querySelector('#meme-image-container');
 
 const getInputText = () => {
  let text = textInput.value; 
@@ -12,6 +14,17 @@ const writeTextInput = () => {
 	})
 }
 
+const ChangeBorder = () => {
+	borderBtns.addEventListener('click' , (event) => {
+		let teste = event.target.className;
+		if (event.target.classList.contains('btn')) {
+			teste = teste.replace(/btn/gi , '');
+		  memeImgContainer.style.border = teste;		  
+		}
+	})
+}
+
 window.onload = () => {
 	writeTextInput();
+	ChangeBorder();
 }
